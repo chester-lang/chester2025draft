f9c76ec200a6b3843567f121698475f1a6784d05
澪 <mio@mio19.uk>
2024-10-12 16:24:52 +1300

    up

---
+++ tyck-platform/jvm/src/main/scala/chester/tyck/ClassFileInspector.scala
@@ -0,0 +1,45 @@
+package chester.tyck
+
+import org.objectweb.asm._
+import org.objectweb.asm.tree._
+import scala.jdk.CollectionConverters._
+import java.nio.file.{Files, Paths}
+
+object ClassFileInspector {
+  def inspectClassFile(classFilePath: String): Unit = {
+    // Read the class file bytes
+    val classBytes = Files.readAllBytes(Paths.get(classFilePath))
+
+    // Create a ClassReader to parse the class bytes
+    val classReader = new ClassReader(classBytes)
+
+    // Use a ClassNode to hold the parsed class
+    val classNode = new ClassNode()
+
+    // Accept the ClassNode to populate it with data
+    classReader.accept(classNode, 0)
+
+    // Print class information
+    println(s"Class: ${classNode.name.replace('/', '.')}")
+
+    // Fields
+    println("Fields:")
+    for (field <- classNode.fields.asInstanceOf[java.util.List[FieldNode]].asScala) {
+      val access = field.access
+      val isPublic = (access & Opcodes.ACC_PUBLIC) != 0
+      if (isPublic) {
+        println(s"  ${field.name}: ${field.desc}")
+      }
+    }
+
+    // Methods
+    println("Methods:")
+    for (method <- classNode.methods.asInstanceOf[java.util.List[MethodNode]].asScala) {
+      val access = method.access
+      val isPublic = (access & Opcodes.ACC_PUBLIC) != 0
+      if (isPublic) {
+        println(s"  ${method.name}${method.desc}")
+      }
+    }
+  }
+}
