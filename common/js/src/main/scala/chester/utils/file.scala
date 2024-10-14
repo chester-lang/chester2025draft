8aa99332fb67db72acec7eaf0558aefa6397ef8e
澪 <mio@mio19.uk>
2024-10-14 21:06:31 +1300

    up

--- common/js/src/main/scala/chester/utils/file.scala
+++ common/js/src/main/scala/chester/utils/file.scala
@@ -1,5 +1,8 @@
 package chester.utils
 
+import typings.node.fsMod
+import typings.node.bufferMod.global.BufferEncoding
+
 def readFileFrom(path: String): String =
-  throw new UnsupportedOperationException("Not implemented yet.")
-def fileExists(path: String): Boolean = false
+    fsMod.readFileSync(path, BufferEncoding.utf8)
+def fileExists(path: String): Boolean = fsMod.existsSync(path)
