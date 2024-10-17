58c1c6ac9d9e643e0ba8eb294d72048e32e80c4c
澪 <mio@mio19.uk>
2024-10-17 23:47:11 +1300

    up

--- i.sh
+++ i.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-curl -fsSL https://moonrepo.dev/install/proto.sh | bash
+check_cmd() {
+  command -v "$1" > /dev/null 2>&1
+  return $?
+}
+proto_cmd=proto
+if ! check_cmd proto; then
+  curl -fsSL https://moonrepo.dev/install/proto.sh | bash
+  proto_cmd="$HOME/.proto/bin/proto"
+  if ! check_cmd "$proto_cmd"; then
+    echo "proto command not found"
+    exit 1
+  fi
+fi
 
-proto plugin add --global chester "https://github.com/chester-lang/chester/raw/refs/heads/main/proto.toml"
+"$proto_cmd" plugin add --global chester "https://github.com/chester-lang/chester/raw/refs/heads/main/proto.toml"
 
-proto install chester
+exec "$proto_cmd" install chester
