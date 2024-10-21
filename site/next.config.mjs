8719179f8bc7791268f381a114d83bc45e38ca2f
澪 <mio@mio19.uk>
2024-10-21 21:19:40 +1300

    up

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -9,43 +9,43 @@ const withNextIntl = createNextIntlPlugin('./app/request.ts');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 function copy_resource(from_file, to_file) {
-    const from = path.join(__dirname, '..', 'resources', from_file);
-    const to = path.join(__dirname, to_file);
-    if (fs.existsSync(from)) {
+  const from = path.join(__dirname, '..', 'resources', from_file);
+  const to = path.join(__dirname, to_file);
+  if (fs.existsSync(from)) {
     fs.copyFileSync(from, to);
-    console.log('Favicon copied successfully');
-} else {
-    console.warn('Favicon not found in resources directory');
-}
+    console.log(from_file + ' copied successfully');
+  } else {
+    console.warn(from_file + ' not found in resources directory');
+  }
 }
 copy_resource('favicon.ico', 'app/favicon.ico');
 copy_resource('chester-logo.svg', 'chester-logo.svg');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
-    output: "export",
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: 'export',
 
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "",
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: '',
 
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-        unoptimized: true,
-    },
-}
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
