f623c63789bc20f498576c692d1656714d72d043
澪 <mio@mio19.uk>
2024-10-24 18:33:19 +1300

    extract site inittermima

--- site/app/[locale]/page.tsx
+++ site/app/[locale]/page.tsx
@@ -2,12 +2,11 @@
 
 import { useEffect, useRef } from 'react';
 import { XTerm } from '@pablo-lion/xterm-react';
-import { startRepl, startReplPty, startReplReadline } from '@/scala/main';
 import { Terminal } from '@xterm/xterm';
-import { Readline } from 'xterm-readline';
 import { useTranslations } from 'next-intl';
 import { useTheme } from '@/components/ThemeContext';
 import Link from 'next/link';
+import { initTerminal } from './terminal';
 
 export default function Home() {
   const t = useTranslations('Home');
@@ -15,58 +14,10 @@ export default function Home() {
   const { theme } = useTheme();
 
   useEffect(() => {
-    // https://stackoverflow.com/questions/66096260/why-am-i-getting-referenceerror-self-is-not-defined-when-i-import-a-client-side/66100185#66100185
-    const initTerminal = async () => {
-      if (xtermRef.current) {
-        const terminal = xtermRef.current.terminal as Terminal;
-        const { WebLinksAddon } = await import('@xterm/addon-web-links');
-        terminal.loadAddon(new WebLinksAddon());
-        const { FitAddon } = await import('@xterm/addon-fit');
-        const fitAddon = new FitAddon();
-        terminal.loadAddon(fitAddon);
-        fitAddon.fit();
-        terminal.focus();
-        if (false) {
-          const { SearchAddon } = await import('@xterm/addon-search');
-          const searchAddon = new SearchAddon();
-          terminal.loadAddon(searchAddon);
-        }
-        const used: string = 'startReplReadline';
-        if (used === 'startRepl') {
-          while (true) {
-            try {
-              await startRepl(terminal);
-            } catch (e) {
-              console.log(e);
-            }
-          }
-        } else if (used === 'startReplPty') {
-          // @ts-expect-error xterm-pty types are not recognized
-          const { openpty } = await import('xterm-pty');
-          const { master, slave } = openpty();
-          terminal.loadAddon(master);
-          while (true) {
-            try {
-              await startReplPty(slave);
-            } catch (e) {
-              console.log(e);
-            }
-          }
-        } else if (used === 'startReplReadline') {
-          const rl = new Readline();
-          terminal.loadAddon(rl);
-          while (true) {
-            try {
-              await startReplReadline(rl);
-            } catch (e: any) {
-              console.log(e);
-              rl.println(e.toString());
-            }
-          }
-        }
-      }
-    };
-    initTerminal();
+    if (xtermRef.current) {
+      const terminal = xtermRef.current.terminal as Terminal;
+      initTerminal(terminal);
+    }
   }, [xtermRef]);
 
   useEffect(() => {
---
+++ site/app/[locale]/terminal.ts
@@ -0,0 +1,52 @@
+import { startRepl, startReplPty, startReplReadline } from '@/scala/main';
+import { Terminal } from '@xterm/xterm';
+import { Readline } from 'xterm-readline';
+
+// https://stackoverflow.com/questions/66096260/why-am-i-getting-referenceerror-self-is-not-defined-when-i-import-a-client-side/66100185#66100185
+export async function initTerminal(terminal: Terminal) {
+  const { WebLinksAddon } = await import('@xterm/addon-web-links');
+  terminal.loadAddon(new WebLinksAddon());
+  const { FitAddon } = await import('@xterm/addon-fit');
+  const fitAddon = new FitAddon();
+  terminal.loadAddon(fitAddon);
+  fitAddon.fit();
+  terminal.focus();
+  if (false) {
+    const { SearchAddon } = await import('@xterm/addon-search');
+    const searchAddon = new SearchAddon();
+    terminal.loadAddon(searchAddon);
+  }
+  const used: string = 'startReplReadline';
+  if (used === 'startRepl') {
+    while (true) {
+      try {
+        await startRepl(terminal);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  } else if (used === 'startReplPty') {
+    // @ts-expect-error xterm-pty types are not recognized
+    const { openpty } = await import('xterm-pty');
+    const { master, slave } = openpty();
+    terminal.loadAddon(master);
+    while (true) {
+      try {
+        await startReplPty(slave);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  } else if (used === 'startReplReadline') {
+    const rl = new Readline();
+    terminal.loadAddon(rl);
+    while (true) {
+      try {
+        await startReplReadline(rl);
+      } catch (e: any) {
+        console.log(e);
+        rl.println(e.toString());
+      }
+    }
+  }
+}
