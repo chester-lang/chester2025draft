287f7c80f7477b9685e9235e50b5668ee3e8bcc1
澪 <mio@mio19.uk>
2024-10-25 11:29:17 +1300

    up

--- compiler213/shared/src/main/scala/chester/backend/scala/Scala.scala
+++ compiler213/shared/src/main/scala/chester/backend/scala/Scala.scala
@@ -7,8 +7,14 @@ import chester.syntax.core._
 object Scala {
   case class ScalaContext()
   def compile(term: core.Term)(implicit ctx: ScalaContext): meta.Tree = term match {
-    case IntegerTerm(i, _) => Lit.Int(i.toInt)
-    case _                 => throw new NotImplementedError(s"not implemented ${term.getClass.getName} $term")
+    case IntegerTerm(i, _) => {
+      require(i.isValidInt)
+      Lit.Int(i.toInt)
+    }
+    case IntTerm(i, _)    => Lit.Int(i)
+    case StringTerm(s, _) => Lit.String(s)
+    case SymbolTerm(s, _) => Lit.Symbol(Symbol(s))
+    case _                => throw new NotImplementedError(s"not implemented ${term.getClass.getName} $term")
   }
 
 }
