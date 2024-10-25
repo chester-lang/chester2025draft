85950a7007541190eeb9228b6d81e9737ab54e5e
澪 <mio@mio19.uk>
2024-10-25 16:49:08 +1300

    save

--- compiler213/shared/src/main/scala/chester/backend/scala/Scala.scala
+++ compiler213/shared/src/main/scala/chester/backend/scala/Scala.scala
@@ -6,7 +6,7 @@ import scala.meta._
 import chester.syntax.core._
 object Scala {
   case class ScalaContext()
-  def compileExpr(term: core.Term)(implicit ctx: ScalaContext = null): meta.Tree = term match {
+  def compileExpr(term: core.Term)(implicit ctx: ScalaContext = null): meta.Term = term match {
     case IntegerTerm(i, _) => {
       require(i.isValidInt)
       Lit.Int(i.toInt)
@@ -18,7 +18,21 @@ object Scala {
     case UnitTerm(_)       => Lit.Unit()
     case _                 => throw new NotImplementedError(s"not implemented ${term.getClass.getName} $term")
   }
+  def compileTy(ty: core.Term)(implicit ctx: ScalaContext = null): meta.Type = ty match {
+    case IntegerType(_) => meta.Type.Name("Int")
+    case IntType(_)     => meta.Type.Name("Int")
+    case StringType(_)  => meta.Type.Name("String")
+    case SymbolType(_)  => meta.Type.Name("Symbol")
+    case _              => throw new NotImplementedError(s"not implemented ${ty.getClass.getName} $ty")
+  }
   def compileStmt(stmt: core.StmtTerm)(implicit ctx: ScalaContext = null): meta.Stat = stmt match {
+    case LetStmtTerm(localv, value, ty, m) =>
+      Defn.Val(
+        mods = Nil,
+        pats = List(meta.Pat.Var(name = meta.Term.Name(localv.name))),
+        decltpe = Some(compileTy(ty)),
+        rhs = compileExpr(value)
+      )
     case _ => throw new NotImplementedError(s"not implemented ${stmt.getClass.getName} $stmt")
   }
 }
