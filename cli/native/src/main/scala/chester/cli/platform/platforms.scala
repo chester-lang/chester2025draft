b455f052fc9e0f5de9a23a8e4c9041045c7facee
澪 <mio@mio19.uk>
2024-10-25 14:46:14 +1300

    no

--- cli/native/src/main/scala/chester/cli/platform/platforms.scala
+++ cli/native/src/main/scala/chester/cli/platform/platforms.scala
@@ -1,7 +1,5 @@
 package chester.cli.platform
 
 def testFunctionalities(): Unit = {
-  println("functionalities test start")
-  chester.scala.Test.callit()
-  println("functionalities test end")
+  ()
 }
