5faa7ffb4d4907b2c5786589450e3851bd5896bb
澪 <mio@mio19.uk>
2024-10-25 22:14:23 +1300

    save

---
+++ interpreter/jvm/src/main/scala/chester/truffle/node/BooleanNode.java
@@ -0,0 +1,33 @@
+package chester.truffle.node;
+
+import chester.syntax.core.BooleanTermC;
+import chester.syntax.core.TermMeta;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.NodeInfo;
+import scala.Option;
+
+@NodeInfo(shortName = "bool")
+public final class BooleanNode extends LiteralNode implements BooleanTermC<TermNode> {
+    final boolean value;
+    final Option<TermMeta> meta;
+
+    public BooleanNode(boolean value, Option<TermMeta> meta) {
+        this.value = value;
+        this.meta = meta;
+    }
+
+    @Override
+    public Object executeGeneric(VirtualFrame frame) {
+        return value;
+    }
+
+    @Override
+    public boolean value() {
+        return value;
+    }
+
+    @Override
+    public Option<TermMeta> meta() {
+        return meta;
+    }
+}
