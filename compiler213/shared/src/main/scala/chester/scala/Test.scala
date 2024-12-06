202f14140e2c91763d1c436e620d412a27d5f7ac
澪 <mio@mio19.uk>
2024-12-07 01:08:46 +1300

    up

--- compiler213/shared/src/main/scala/chester/scala/Test.scala
+++ compiler213/shared/src/main/scala/chester/scala/Test.scala
@@ -3,8 +3,7 @@ package chester.scala
 import chester.backend.scala.Scala
 
 import scala.meta
-
-
+import scala.meta._
 import chester.syntax.core.simple._
 object Test {
   val test: meta.Term.Apply = q"function(argument)"
