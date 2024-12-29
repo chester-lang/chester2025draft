0418f5d665760ce9545d17b557244253ca2c7907
澪 <mio@mio19.uk>
2024-12-29 10:20:53 +0800

    up

--- interpreter/jvm/src/main/scala/chester/truffle/ChesterLang.java
+++ interpreter/jvm/src/main/scala/chester/truffle/ChesterLang.java
@@ -4,7 +4,11 @@ import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.TruffleLanguage.Env;
 import com.oracle.truffle.api.TruffleLanguage.ParsingRequest;
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RootNode;
+import chester.syntax.core.truffle.*;
 
 @TruffleLanguage.Registration(id = "cst", name = "Chester")
 public final class ChesterLang extends TruffleLanguage<ChesterContext> {
@@ -16,11 +20,27 @@ public final class ChesterLang extends TruffleLanguage<ChesterContext> {
     }
     @Override
     protected CallTarget parse(ParsingRequest request) throws Exception {
-        return Utils.parse(request);
+        return Utils.parse(this, request);
     }
 
     @Override
     protected ChesterContext createContext(Env env) {
         return new ChesterContext();
     }
+
+
+    public static class ChesterRootNode  extends RootNode {
+        @Child Term body;
+
+        ChesterRootNode(ChesterLang lang, Term body) {
+            super(lang, new FrameDescriptor());
+            this.body = body;
+        }
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return body.executeGeneric(frame);
+        }
+    }
+
 }
--- interpreter/jvm/src/main/scala/chester/truffle/Utils.scala
+++ interpreter/jvm/src/main/scala/chester/truffle/Utils.scala
@@ -1,18 +1,24 @@
 package chester.truffle
 
 import chester.error.unreachable
-import com.oracle.truffle.api.{CallTarget, TruffleLanguage}
+import com.oracle.truffle.api.{CallTarget, Truffle, TruffleLanguage}
 import chester.reader.*
+import chester.syntax.core.*
+import chester.truffle.ChesterLang.ChesterRootNode
 import chester.tyck.*
 
 object Utils {
   @throws[Exception]
-  def parse(request: TruffleLanguage.ParsingRequest): CallTarget = {
+  def parse(lang: ChesterLang, request: TruffleLanguage.ParsingRequest): CallTarget = {
     ChesterReader.parseTopLevel(FileNameAndContent(request.getSource.getPath, request.getSource.getCharacters.toString)) match {
       case Left(err) => ???
       case Right(parsedBlock) =>
         Tycker.check(parsedBlock) match {
-          case TyckResult.Success(result, _, _)    => ???
+          case TyckResult.Success(result, _, _) => {
+            val t: Term = result.wellTyped
+            val root = new ChesterRootNode(lang, t)
+            root.getCallTarget
+          }
           case TyckResult.Failure(errors, _, _, _) => ???
           case _                                   => unreachable()
         }
