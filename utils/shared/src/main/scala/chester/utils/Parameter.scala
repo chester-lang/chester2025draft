c9104df7c738b984ab2280290dfe2d16954475c8
澪 <mio@mio19.uk>
2024-12-30 08:57:04 +0800

    up

---
+++ utils/shared/src/main/scala/chester/utils/Parameter.scala
@@ -0,0 +1,21 @@
+package chester.utils
+
+import java.util.function.Supplier
+
+class Parameter[T](default: => (T | Null) = null) {
+  val tl: ThreadLocal[T | Null] = ThreadLocal.withInitial(() => default)
+
+  def withValue[U](value: T)(block: => U): U = {
+    val previousValue = tl.get()
+    try {
+      tl.set(value)
+      block
+    } finally {
+      tl.set(previousValue)
+    }
+  }
+  def get: T = tl.get() match {
+    case null  => throw new IllegalStateException("Value is null")
+    case value => value.asInstanceOf[T]
+  }
+}
