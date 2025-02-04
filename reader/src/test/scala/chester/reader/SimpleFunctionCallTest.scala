b66a522e08104fbd371c238a31bbda51a8465685
澪 <mio@mio19.uk>
2025-02-05 02:32:48 +1100

    moret e

--- reader/src/test/scala/chester/reader/SimpleFunctionCallTest.scala
+++ reader/src/test/scala/chester/reader/SimpleFunctionCallTest.scala
@@ -29,4 +29,71 @@ class SimpleFunctionCallTest extends FunSuite {
     )
     parseAndCheck(input, expected)
   }
+
+  test("function call with one arg") {
+    val input = "foo(1)"
+    val expected = FunctionCall(
+      Identifier("foo", meta = None),
+      Tuple(Vector(IntegerLiteral(1, meta = None)), meta = None),
+      meta = None
+    )
+    parseAndCheck(input, expected)
+  }
+
+  test("function call with multiple args") {
+    val input = "foo(1, 2, 3)"
+    val expected = FunctionCall(
+      Identifier("foo", meta = None),
+      Tuple(
+        Vector(
+          IntegerLiteral(1, meta = None),
+          IntegerLiteral(2, meta = None),
+          IntegerLiteral(3, meta = None)
+        ),
+        meta = None
+      ),
+      meta = None
+    )
+    parseAndCheck(input, expected)
+  }
+
+  test("nested function calls") {
+    val input = "foo(bar())"
+    val expected = FunctionCall(
+      Identifier("foo", meta = None),
+      Tuple(
+        Vector(
+          FunctionCall(
+            Identifier("bar", meta = None),
+            Tuple(Vector.empty, meta = None),
+            meta = None
+          )
+        ),
+        meta = None
+      ),
+      meta = None
+    )
+    parseAndCheck(input, expected)
+  }
+
+  test("function call with mixed arg types") {
+    val input = """foo(1, "test", bar())"""
+    val expected = FunctionCall(
+      Identifier("foo", meta = None),
+      Tuple(
+        Vector(
+          IntegerLiteral(1, meta = None),
+          StringLiteral("test", meta = None),
+          FunctionCall(
+            Identifier("bar", meta = None),
+            Tuple(Vector.empty, meta = None),
+            meta = None
+          )
+        ),
+        meta = None
+      ),
+      meta = None
+    )
+    parseAndCheck(input, expected)
+  }
 } 
