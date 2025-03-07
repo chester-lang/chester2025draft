8f2509d5653b3836ad170e9599fdcdec5053a6ee
澪 <mio@mio19.uk>
2025-03-08 02:53:59 +1300

    Simplify ChesterReaderV2: Extract common setup logic and remove unused imports

--- reader/src/main/scala/chester/readerv2/ChesterReaderV2.scala
+++ reader/src/main/scala/chester/readerv2/ChesterReaderV2.scala
@@ -3,10 +3,10 @@ package chester.readerv2
 import chester.error.Reporter
 import chester.reader.{FileNameAndContent, ParseError, SourceOffset}
 import chester.syntax.concrete.Expr
-import chester.error.{Pos, RangeInFile, SourcePos}
 
 object ChesterReaderV2 {
-  def parseExpr(source: FileNameAndContent): Either[ParseError, Expr] = {
+  // Helper method to set up tokenizer and lexer with common logic
+  private def setupLexer(source: FileNameAndContent, ignoreLocation: Boolean = false): (LexerV2, LexerState) = {
     given reporter: Reporter[ParseError] = new Reporter[ParseError] {
       def apply(error: ParseError): Unit = {}
       def const(e: ParseError): Reporter[ParseError] = this
@@ -14,20 +14,20 @@ object ChesterReaderV2 {
     val sourceOffset = SourceOffset(source)
     val tokenizer = new Tokenizer(sourceOffset)
     val tokens = tokenizer.tokenize()
-    val lexer = new LexerV2(tokens, sourceOffset, false)
-    lexer.parseExpr(LexerState(tokens.toVector, 0)).map(_._1)
+    val lexer = new LexerV2(tokens, sourceOffset, ignoreLocation)
+    val initialState = LexerState(tokens.toVector, 0)
+    (lexer, initialState)
+  }
+
+  def parseExpr(source: FileNameAndContent): Either[ParseError, Expr] = {
+    val (lexer, state) = setupLexer(source)
+    lexer.parseExpr(state).map(_._1)
   }
 
   def parseExprList(source: FileNameAndContent): Either[ParseError, Vector[Expr]] = {
-    given reporter: Reporter[ParseError] = new Reporter[ParseError] {
-      def apply(error: ParseError): Unit = {}
-      def const(e: ParseError): Reporter[ParseError] = this
-    }
-    val sourceOffset = SourceOffset(source)
-    val tokenizer = new Tokenizer(sourceOffset)
-    val tokens = tokenizer.tokenize()
-    val lexer = new LexerV2(tokens, sourceOffset, false)
-    SourcePos(sourceOffset, RangeInFile(Pos.zero, Pos.zero))
-    lexer.parseExprList(LexerState(tokens.toVector, 0)).map(_._1)
+    val (lexer, state) = setupLexer(source)
+    lexer.parseExprList(state).map(_._1)
   }
+
+  // Add other parsing methods here with the same pattern
 }
