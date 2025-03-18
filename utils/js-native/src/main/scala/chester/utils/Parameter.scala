fd417d762e7f57b875a2554a294f7ff1de93ec15
澪 <mio@mio19.uk>
2025-03-18 23:05:38 +1300

    Parameter

---
+++ utils/js-native/src/main/scala/chester/utils/Parameter.scala
@@ -0,0 +1,20 @@
+package chester.utils
+
+// TODO: Support Scala Native multithreading
+class Parameter[T](default: T | Null) {
+  var tl: T | Null = default
+
+  def withValue[U](value: T)(block: => U): U = {
+    val previousValue = tl
+    try {
+      tl = value
+      block
+    } finally {
+      tl = previousValue
+    }
+  }
+  def get: T = tl match {
+    case null  => throw new IllegalStateException("Value is null")
+    case value => value.asInstanceOf[T]
+  }
+}
