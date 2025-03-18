416e1f3e6361ca70244893f4497e1f5cc213465b
澪 <mio@mio19.uk>
2025-03-18 23:23:19 +1300

    up

--- site/i18n/navigation.ts
+++ site/i18n/navigation.ts
@@ -1,5 +1,5 @@
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+import { createNavigation } from 'next-intl/navigation';
 import { SUPPORTED_LOCALES } from '@/i18n';
 const locales = SUPPORTED_LOCALES;
 export const { Link, useRouter, usePathname, redirect } =
-  createSharedPathnamesNavigation({ locales });
+createNavigation({ locales });
