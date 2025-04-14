df30cc5a7b0c6c97cb9af460bffa045c3ad748fa
澪 <mio@mio19.uk>
2025-04-15 10:41:21 +1200

    Fix object parsing in LexerV2 to handle OpSeq expressions in object clauses

--- reader/src/main/scala/chester/readerv2/ChesterReaderV2.scala
+++ reader/src/main/scala/chester/readerv2/ChesterReaderV2.scala
@@ -4,23 +4,22 @@ import chester.syntax.concrete.Expr
 
 object ChesterReaderV2 {
   // Helper method to set up tokenizer and lexer with common logic
-  private def setupLexer(source: FileNameAndContent, ignoreLocation: Boolean = false): (LexerV2, LexerState) = {
+  private def setupLexer(source: FileNameAndContent, ignoreLocation: Boolean = false): LexerV2 = {
     val sourceOffset = SourceOffset(source)
     val tokenizer = new Tokenizer(sourceOffset)
     val tokens = tokenizer.tokenize()
-    val lexer = new LexerV2(sourceOffset, ignoreLocation)
-    val initialState = LexerState(tokens.toVector, 0)
-    (lexer, initialState)
+    val initialState = LexerState(sourceOffset, tokens.toVector, 0)
+    new LexerV2(initialState, ignoreLocation)
   }
 
   def parseExpr(source: FileNameAndContent): Either[ParseError, Expr] = {
-    val (lexer, state) = setupLexer(source)
-    lexer.parseExpr(state).map(_._1)
+    val lexer = setupLexer(source)
+    lexer.parseExpr()
   }
 
   def parseExprList(source: FileNameAndContent): Either[ParseError, Vector[Expr]] = {
-    val (lexer, state) = setupLexer(source)
-    lexer.parseExprList(state).map(_._1)
+    val lexer = setupLexer(source)
+    lexer.parseExprList()
   }
 
   // Add other parsing methods here with the same pattern
