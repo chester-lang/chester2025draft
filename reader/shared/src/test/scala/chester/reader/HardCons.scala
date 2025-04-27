8545cea9680b21aa402e544b119d792ab7142dde
澪 <mio@mio19.uk>
2025-04-27 20:45:00 +1200

    cons

---
+++ reader/shared/src/test/scala/chester/reader/HardCons.scala
@@ -0,0 +1,51 @@
+package chester.reader
+
+import chester.syntax.concrete.*
+import munit.FunSuite
+
+class HardCons extends FunSuite{
+
+  test("Cons { head = 0, tail = Nil }") {
+    val input = "Cons { head = 0, tail = Nil }"
+    val expected =
+      FunctionCall(
+        function = Identifier(
+          name = "Cons",
+          meta = None
+        ),
+        telescope = Tuple(
+          terms = Vector(
+            ObjectExpr(
+              clauses = Vector(
+                ObjectExprClause(
+                  key = Identifier(
+                    name = "head",
+                    meta = None
+                  ),
+                  value = IntegerLiteral(
+                    value = 0,
+                    meta = None
+                  )
+                ),
+                ObjectExprClause(
+                  key = Identifier(
+                    name = "tail",
+                    meta = None
+                  ),
+                  value = Identifier(
+                    name = "Nil",
+                    meta = None
+                  )
+                )
+              ),
+              meta = None
+            )
+          ),
+          meta = None
+        ),
+        meta = None
+      )
+    parseAndCheckV1(input, expected)
+  }
+
+}
