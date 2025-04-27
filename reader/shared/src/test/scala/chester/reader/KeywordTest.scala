d803424648b9985061944b68fd9d9b5fef8cd8d5
澪 <mio@mio19.uk>
2025-04-27 20:06:43 +1200

    keyword test

---
+++ reader/shared/src/test/scala/chester/reader/KeywordTest.scala
@@ -0,0 +1,18 @@
+package chester.reader
+
+import chester.syntax.concrete.Keyword
+import munit.FunSuite
+
+class KeywordTest extends FunSuite {
+
+  test("parse a keyword") {
+    val input = "#\uD83D\uDE3F\uD83D\uDE3F"
+    val expected = Keyword(
+      key = "\ud83d\ude3f\ud83d\ude3f",
+      telescope = Vector(),
+      meta = None
+    )
+    parseAndCheckV1(input, expected)
+  }
+
+}
