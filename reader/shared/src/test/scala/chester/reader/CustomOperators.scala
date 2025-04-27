6acc8b7b75a137cfe50c7d525019e04673254cd4
澪 <mio@mio19.uk>
2025-04-27 20:55:23 +1200

    CustomOperators

---
+++ reader/shared/src/test/scala/chester/reader/CustomOperators.scala
@@ -0,0 +1,27 @@
+package chester.reader
+
+import chester.syntax.concrete.*
+import munit.FunSuite
+
+class CustomOperators extends FunSuite{
+
+  test("?hole") {
+    val input = "?hole"
+    val expected =
+      OpSeq(
+        seq = Vector(
+          Identifier(
+            name = "?",
+            meta = None
+          ),
+          Identifier(
+            name = "hole",
+            meta = None
+          )
+        ),
+        meta = None
+      )
+    parseAndCheckV1(input, expected)
+  }
+
+}
