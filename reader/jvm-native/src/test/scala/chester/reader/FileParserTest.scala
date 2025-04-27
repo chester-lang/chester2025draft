d7c168f87e6f1bbf7356412d54d4a9582fa38bd2
澪 <mio@mio19.uk>
2025-04-27 14:54:20 +1200

    qwq

--- reader/jvm-native/src/test/scala/chester/reader/FileParserTest.scala
+++ reader/jvm-native/src/test/scala/chester/reader/FileParserTest.scala
@@ -4,6 +4,8 @@ import chester.syntax.concrete.*
 import munit.FunSuite
 import upickle.default.*
 import chester.i18n.*
+import chester.readerv2.ChesterReaderV2
+import chester.readerv2.LexerV2.DEBUG
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
@@ -18,32 +20,34 @@ class FileParserTest extends FunSuite {
 
       val expectedExists = Files.exists(expectedFile)
 
-      ChesterReader
-        .parseTopLevel(
-          FilePath(inputFile.toString),
-          ignoreLocation = true
-        )
-        .fold(
-          error => fail(t"Failed to parse file: $inputFile, error: $error"),
-          { parsedBlock =>
-            assertEquals(read[Expr](write[Expr](parsedBlock)), parsedBlock)
-            assertEquals(readBinary[Expr](writeBinary[Expr](parsedBlock)), parsedBlock)
-            val actual: String = pprint
-              .apply(parsedBlock, width = 128, height = Integer.MAX_VALUE)
-              .plainText
-              .replace("\r\n", "\n")
-
-            if (!expectedExists) {
-              Files.write(expectedFile, actual.getBytes)
-              println(t"Created expected file: $expectedFile")
-            } else {
-              val expected = Files
-                .readString(expectedFile, StandardCharsets.UTF_8)
+      DEBUG.withValue(true) {
+        ChesterReader
+          .parseTopLevel(
+            FilePath(inputFile.toString),
+            ignoreLocation = true
+          )
+          .fold(
+            error => fail(t"Failed to parse file: $inputFile, error: $error"),
+            { parsedBlock =>
+              assertEquals(read[Expr](write[Expr](parsedBlock)), parsedBlock)
+              assertEquals(readBinary[Expr](writeBinary[Expr](parsedBlock)), parsedBlock)
+              val actual: String = pprint
+                .apply(parsedBlock, width = 128, height = Integer.MAX_VALUE)
+                .plainText
                 .replace("\r\n", "\n")
-              assertEquals(actual, expected)
+
+              if (!expectedExists) {
+                Files.write(expectedFile, actual.getBytes)
+                println(t"Created expected file: $expectedFile")
+              } else {
+                val expected = Files
+                  .readString(expectedFile, StandardCharsets.UTF_8)
+                  .replace("\r\n", "\n")
+                assertEquals(actual, expected)
+              }
             }
-          }
-        )
+          )
+      }
     }
   }
 }
