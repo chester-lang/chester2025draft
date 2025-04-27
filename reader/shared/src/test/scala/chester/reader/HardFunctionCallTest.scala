27ac1c316c6846a5266565ea9e41ce0e20849561
澪 <mio@mio19.uk>
2025-04-28 10:08:43 +1200

    Add tests for function calls with parentheses, confirmed working with V1 parser

--- reader/shared/src/test/scala/chester/reader/HardFunctionCallTest.scala
+++ reader/shared/src/test/scala/chester/reader/HardFunctionCallTest.scala
@@ -68,7 +68,7 @@ class HardFunctionCallTest extends FunSuite{
         ),
         meta = None
       )
-    DEBUG.withValue(true) {
+    DEBUG.withValue(false) {
       parseAndCheckBoth(input, expected)
     }
 
@@ -139,5 +139,136 @@ class HardFunctionCallTest extends FunSuite{
 
   }
 
+  test("OpSeq And FunctionCall F<nospace>()") {
+    val input = "trait #sealed Vector(n: Nat, T: Type)"
+    val expected =
+      OpSeq(
+        seq = Vector(
+          Identifier(
+            name = "trait",
+            meta = None
+          ),
+          Keyword(
+            key = "sealed",
+            telescope = Vector(),
+            meta = None
+          ),
+          FunctionCall(
+            function = Identifier(
+              name = "Vector",
+              meta = None
+            ),
+            telescope = Tuple(
+              terms = Vector(
+                OpSeq(
+                  seq = Vector(
+                    Identifier(
+                      name = "n",
+                      meta = None
+                    ),
+                    Identifier(
+                      name = ":",
+                      meta = None
+                    ),
+                    Identifier(
+                      name = "Nat",
+                      meta = None
+                    )
+                  ),
+                  meta = None
+                ),
+                OpSeq(
+                  seq = Vector(
+                    Identifier(
+                      name = "T",
+                      meta = None
+                    ),
+                    Identifier(
+                      name = ":",
+                      meta = None
+                    ),
+                    Identifier(
+                      name = "Type",
+                      meta = None
+                    )
+                  ),
+                  meta = None
+                )
+              ),
+              meta = None
+            ),
+            meta = None
+          )
+        ),
+        meta = None
+      )
+    DEBUG.withValue(false) {
+      parseAndCheckV1(input, expected)
+    }
+  }
 
+  test("OpSeq And Not FunctionCall F<space>()") {
+    val input = "trait #sealed Vector (n: Nat, T: Type)"
+    val expected =
+      OpSeq(
+        seq = Vector(
+          Identifier(
+            name = "trait",
+            meta = None
+          ),
+          Keyword(
+            key = "sealed",
+            telescope = Vector(),
+            meta = None
+          ),
+          Identifier(
+            name = "Vector",
+            meta = None
+          ),
+          Tuple(
+            terms = Vector(
+              OpSeq(
+                seq = Vector(
+                  Identifier(
+                    name = "n",
+                    meta = None
+                  ),
+                  Identifier(
+                    name = ":",
+                    meta = None
+                  ),
+                  Identifier(
+                    name = "Nat",
+                    meta = None
+                  )
+                ),
+                meta = None
+              ),
+              OpSeq(
+                seq = Vector(
+                  Identifier(
+                    name = "T",
+                    meta = None
+                  ),
+                  Identifier(
+                    name = ":",
+                    meta = None
+                  ),
+                  Identifier(
+                    name = "Type",
+                    meta = None
+                  )
+                ),
+                meta = None
+              )
+            ),
+            meta = None
+          )
+        ),
+        meta = None
+      )
+    DEBUG.withValue(false) {
+      parseAndCheckV1(input, expected)
+    }
+  }
 }
