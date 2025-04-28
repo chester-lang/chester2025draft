4e65f3f788594a80439d73085d08fd48069d0fa2
澪 <mio@mio19.uk>
2025-04-28 14:15:02 +1200

    v2 has problem here

---
+++ reader/shared/src/test/scala/chester/reader/HardTopLevelTest.scala
@@ -0,0 +1,71 @@
+package chester.reader
+
+import chester.syntax.concrete.*
+import chester.readerv2.ReaderV2.DEBUG
+import munit.FunSuite
+
+class HardTopLevelTest extends FunSuite{
+  test("a def"){
+
+    val input = """
+                  |data Dog extends AnimalType {
+                  |  wo: String;
+                  |}
+                  |""".stripMargin
+    val expected =
+      Block(
+        statements = Vector(
+          OpSeq(
+            seq = Vector(
+              Identifier(
+                name = "data",
+                meta = None
+              ),
+              Identifier(
+                name = "Dog",
+                meta = None
+              ),
+              Identifier(
+                name = "extends",
+                meta = None
+              ),
+              Identifier(
+                name = "AnimalType",
+                meta = None
+              ),
+              Block(
+                statements = Vector(
+                  OpSeq(
+                    seq = Vector(
+                      Identifier(
+                        name = "wo",
+                        meta = None
+                      ),
+                      Identifier(
+                        name = ":",
+                        meta = None
+                      ),
+                      Identifier(
+                        name = "String",
+                        meta = None
+                      )
+                    ),
+                    meta = None
+                  )
+                ),
+                result = None,
+                meta = None
+              )
+            ),
+            meta = None
+          )
+        ),
+        result = None,
+        meta = None
+      )
+    DEBUG.withValue(true) {
+      parseAndCheckTopLevelV1(input, expected)
+    }
+  }
+
+}
