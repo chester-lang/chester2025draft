e1b2230d30e6c255ccfcc5cde2d1564b4e65fe33
澪 <mio@mio19.uk>
2025-05-10 23:16:55 +1200

    try to get a more clean architecture in chester.elab

--- semantics/shared/src/main/scala/chester/elab/Constrain.scala
+++ semantics/shared/src/main/scala/chester/elab/Constrain.scala
@@ -3,14 +3,28 @@ package chester.elab
 import chester.syntax.core.Term
 
 /** implementations should be case object */
-open trait ConstrainKind {
+open trait Kind {
   override def toString: String = super.toString
-  type Rule <: Constrain
+  type ConstrainType <: Constrain
 }
 
 
-open trait Constrain(val kind: ConstrainKind) {
+open trait Constrain(val kind: Kind) {
   def show: Vector[Term]
 }
 
 case class CellId[T <: Term]()
+
+open trait Handler(val kind: Kind) {
+  def run: Unit
+  def zonk(level: ZonkLevel): Unit
+}
+
+enum ZonkLevel extends Enum[ZonkLevel] {
+  case Least
+  case Upper
+}
+extension (x: ZonkLevel) {
+  // depend on this assumption: the first one should be 0
+  def precedence: Int = x.ordinal
+}
--- semantics/shared/src/main/scala/chester/elab/MergeSimple.scala
+++ semantics/shared/src/main/scala/chester/elab/MergeSimple.scala
@@ -2,8 +2,8 @@ package chester.elab
 
 import chester.syntax.core.Term
 
-case object MergeSimple extends ConstrainKind {
-  type Rule = MergeSimple[?]
+case object MergeSimple extends Kind {
+  type ConstrainType = MergeSimple[?]
 }
 
 case class MergeSimple[T <: Term](a: CellId[T], b: CellId[T]) extends Constrain(MergeSimple) {
