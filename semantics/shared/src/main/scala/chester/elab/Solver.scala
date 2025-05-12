3fae7f3a54fbd3cb5a72bb0ba48c1fbdb40a0bae
澪 <mio@mio19.uk>
2025-05-12 17:44:29 +1200

    saving chester.elab

--- semantics/shared/src/main/scala/chester/elab/Solver.scala
+++ semantics/shared/src/main/scala/chester/elab/Solver.scala
@@ -4,6 +4,7 @@ import chester.uniqid.Uniqid
 
 import java.util.concurrent.ForkJoinPool
 import java.util.concurrent.atomic.AtomicReference
+import scala.annotation.tailrec
 import scala.collection.concurrent.TrieMap
 
 trait HandlerConf {
@@ -46,18 +47,18 @@ private trait BasicSolverOps extends SolverOps {
 
 }
 
-case class WaitingConstraint(vars: Vector[CellId[?]],x: Constraint)
+case class WaitingConstraint(vars: Vector[CellId[?]], x: Constraint)
 
 final class ConcurrentSolver[Ops] private (val conf: HandlerConf) extends BasicSolverOps {
   private val pool = new ForkJoinPool()
   private val delayedConstraints = new AtomicReference(Vector[WaitingConstraint]())
   private val failedConstraints = new AtomicReference(Vector[Constraint]())
-  //implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(pool)
+  // implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(pool)
 
   override def stable: Boolean = {
-    if(delayedConstraints.get().nonEmpty) return false
-    if(pool.isShutdown) return true
-    if(pool.isQuiescent) {
+    if (delayedConstraints.get().nonEmpty) return false
+    if (pool.isShutdown) return true
+    if (pool.isQuiescent) {
       val tasks = pool.shutdownNow()
       assume(tasks.isEmpty)
       return true
@@ -65,26 +66,36 @@ final class ConcurrentSolver[Ops] private (val conf: HandlerConf) extends BasicS
     return false
   }
 
-  override def addConstraint(x: Constraint): Unit = {
-    pool.execute(() => {
-      val handler = conf.getHandler(x.kind).getOrElse{throw new IllegalStateException("no handler")}
+  override def addConstraint(x: Constraint): Unit =
+    pool.execute { () =>
+      val handler = conf.getHandler(x.kind).getOrElse(throw new IllegalStateException("no handler"))
       val result = handler.run(x.asInstanceOf[handler.kind.ConstraintType])
       result match {
         case Result.Done => ()
-        case Result.Failed => {
-          val _ = failedConstraints.getAndUpdate(_.appended(x))}
-        case Result.Waiting(vars) => {
-          val delayed = WaitingConstraint(vars,x)
+        case Result.Failed =>
+          val _ = failedConstraints.getAndUpdate(_.appended(x))
+        case Result.Waiting(vars) =>
+          val delayed = WaitingConstraint(vars, x)
           val _ = delayedConstraints.getAndUpdate(_.appended(delayed))
-        }
       }
-    })
+    }
+
+  @tailrec
+  override def fill[T](id: CellId[T], value: T): Unit = {
+    val current = id.storeRef.get()
+    val read = current.readStable
+    if (read.isDefined) {
+      if (value == read.get) return
+      throw new IllegalStateException("cannot overwrite stable value")
+    }
+    if(!id.storeRef.compareAndSet(current, current.fill(value))) {
+      return fill(id, value)
+    }
+    // TODO: trigger some delayed contrains
   }
 }
 
-final class SinglethreadSolver[Ops] {
-
-}
+final class SinglethreadSolver[Ops] {}
 
 trait SolverOps {
   def hasStableValue[T](id: CellId[T]): Boolean
@@ -97,4 +108,6 @@ trait SolverOps {
   def stable: Boolean
 
   def addConstraint(x: Constraint): Unit
+
+  def fill[T](id: CellId[T], value: T): Unit
 }
