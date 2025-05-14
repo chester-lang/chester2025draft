c652ab722ce157c73c26f1b96afe21d0fb26809b
澪 <mio@mio19.uk>
2025-05-14 22:45:57 +1200

    sad

--- semantics/shared/src/main/scala/chester/utils/elab/ProceduralSolver.scala
+++ semantics/shared/src/main/scala/chester/utils/elab/ProceduralSolver.scala
@@ -5,8 +5,8 @@ import chester.utils.cell.{CellContent, CellContentR}
 import scala.collection.mutable
 
 final class ProceduralCell[+A, -B, C <: CellContent[A, B]](
-                                                            initialValue: C
-                                                          ) extends Cell[A, B, C] {
+    initialValue: C
+) extends Cell[A, B, C] {
   var store = initialValue
 }
 
@@ -17,39 +17,51 @@ object ProceduralSolver extends SolverFactory {
 final class ProceduralSolver[Ops](val conf: HandlerConf[Ops])(using Ops) extends BasicSolverOps {
   given SolverOps = this
   private val todo = mutable.Queue[Constraint]()
-  private val delayedConstraints = mutable.ArrayBuffer[WaitingConstraint]()
+  private var delayedConstraints = mutable.ArrayBuffer[WaitingConstraint]()
   private val failedConstraints = mutable.ArrayBuffer[Constraint]()
+  private val updatedCells = mutable.ArrayBuffer[CellAny]()
 
   implicit inline def thereAreAllProcedural[A, B, C <: CellContent[A, B]](inline x: Cell[A, B, C]): ProceduralCell[A, B, C] =
     x.asInstanceOf[ProceduralCell[A, B, C]]
-
 
   override protected def peakCell[T](id: CellR[T]): CellContentR[T] = id.store
 
   override protected def updateCell[A, B](id: CellOf[A, B], f: CellContent[A, B] => CellContent[A, B]): Unit = {
     id.store = f(id.store)
-    // TODO: find relevant delayedConstraints and move them to todo
+    updatedCells.append(id)
   }
 
-  //@tailrec
+  // @tailrec
   override def run(): Unit = {
-    while(todo.nonEmpty) {
-      val c = todo.dequeue()
-      val handler = conf.getHandler(c.kind).getOrElse(throw new IllegalStateException("no handler"))
-      val result = handler.run(c.asInstanceOf[handler.kind.Of])
-      result match {
-        case Result.Done =>
+    while (todo.nonEmpty) {
+      var heuristics: Int = 1
+      while (todo.nonEmpty && heuristics < 32) {
+        heuristics += 1
+        val c = todo.dequeue()
+        val handler = conf.getHandler(c.kind).getOrElse(throw new IllegalStateException("no handler"))
+        val result = handler.run(c.asInstanceOf[handler.kind.Of])
+        result match {
+          case Result.Done =>
           // do nothing
-        case Result.Waiting(vars*) =>
-          delayedConstraints.append(WaitingConstraint(vars.toVector, c))
-        case Result.Failed =>
-          failedConstraints.append(c)
+          case Result.Waiting(vars*) =>
+            delayedConstraints.append(WaitingConstraint(vars.toVector, c))
+          case Result.Failed =>
+            failedConstraints.append(c)
+        }
+      }
+      if (delayedConstraints.nonEmpty) {
+        // pick those has waiting vars in updatedCells
+        val _ = delayedConstraints.filterInPlace { c =>
+          val call = c.vars.exists(updatedCells.contains)
+          if (call) todo.enqueue(c.x)
+          !call
+        }
+        updatedCells.clear()
       }
     }
-    while(delayedConstraints.nonEmpty) {
+    while (delayedConstraints.nonEmpty)
       // TODO: defaulting
       ???
-    }
   }
 
   override def stable: Boolean = delayedConstraints.isEmpty && todo.isEmpty
