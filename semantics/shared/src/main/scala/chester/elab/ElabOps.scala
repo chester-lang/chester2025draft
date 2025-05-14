d8a787a704226d69191d891620b160dc89534f2c
澪 <mio@mio19.uk>
2025-05-14 17:20:51 +1200

    saving chester.elab

---
+++ semantics/shared/src/main/scala/chester/elab/ElabOps.scala
@@ -0,0 +1,13 @@
+package chester.elab
+
+import chester.error.{Reporter, TyckProblem}
+import chester.tyck.api.SemanticCollector
+import scala.language.implicitConversions
+
+case class ElabOps(reporter: Reporter[TyckProblem], collector: SemanticCollector) {
+
+}
+
+implicit def getReporter(x: ElabOps): Reporter[TyckProblem] = x.reporter
+implicit def getCollector(x: ElabOps): SemanticCollector = x.collector
+
