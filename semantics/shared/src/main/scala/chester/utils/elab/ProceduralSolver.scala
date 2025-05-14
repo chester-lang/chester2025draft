6b614b45b18635926614c848f930f0c4e5b91019
澪 <mio@mio19.uk>
2025-05-14 23:32:14 +1200

    here

--- semantics/shared/src/main/scala/chester/utils/elab/ProceduralSolver.scala
+++ semantics/shared/src/main/scala/chester/utils/elab/ProceduralSolver.scala
@@ -60,14 +60,6 @@ final class ProceduralSolver[Ops](val conf: HandlerConf[Ops])(using Ops) extends
         updatedCells.clear()
       }
     }
-    if (delayedConstraints.nonEmpty) {
-      val _ = delayedConstraints.filterInPlace { c =>
-        val call = c.vars.exists(updatedCells.contains)
-        if (call) todo.enqueue(c.x)
-        !call
-      }
-      updatedCells.clear()
-    }
     var defaults = DefaultingLevel.Values
     var nothingChanged = true
     while (nothingChanged && defaults.nonEmpty) {
@@ -105,6 +97,14 @@ final class ProceduralSolver[Ops](val conf: HandlerConf[Ops])(using Ops) extends
     }
     if (defaults.isEmpty && nothingChanged) {
       throw new IllegalStateException("cannot finish some constraints")
+    }
+    if (delayedConstraints.nonEmpty) {
+      val _ = delayedConstraints.filterInPlace { c =>
+        val call = c.vars.exists(updatedCells.contains)
+        if (call) todo.enqueue(c.x)
+        !call
+      }
+      updatedCells.clear()
     }
     if (!stable) return run()
   }
