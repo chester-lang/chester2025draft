927e56da5cbabf7b69e1b810ed0d959e6acc9072
Mio <mio@mio19.uk>
2025-05-14 19:45:57 +1200

    saving elab

--- semantics/shared/src/main/scala/chester/elab/tryUnify.scala
+++ semantics/shared/src/main/scala/chester/elab/tryUnify.scala
@@ -5,35 +5,37 @@ import chester.tyck.Context
 import chester.utils.elab.*
 import spire.math.Trilean
 
+import scala.language.postfixOps
+
 extension (sub: CellRWOr[Term]) {
   def `<:?`(supertype: CellRWOr[Term])(using ElabOps, SolverOps, Context): Trilean = tryUnify(supertype, sub)
   def `<:!`(supertype: CellRWOr[Term])(using ElabOps, SolverOps, Context): Unit = SolverOps.addConstraint(Unify(supertype, sub))
 }
 
 def tryUnify(lhs: CellRWOr[Term], rhs: CellRWOr[Term])(using ElabOps, SolverOps, Context): Trilean = {
-  if(lhs == rhs) return Trilean.True
+  if (lhs == rhs) return Trilean.True
   val lhsV = toTerm(lhs)
-    val rhsV = toTerm(rhs)
+  val rhsV = toTerm(rhs)
   if (lhsV == rhsV) return Trilean.True
-  if(lhsV.isInstanceOf[MetaTerm] || rhsV.isInstanceOf[MetaTerm]) {
+  if (lhsV.isInstanceOf[MetaTerm] || rhsV.isInstanceOf[MetaTerm]) {
     return Trilean.Unknown
   }
   (lhsV, rhsV) match {
     case (IntegerType(_), IntegerType(_)) => Trilean.True
-    case (IntegerType(_), _: SimpleType) => Trilean.False
-    case (IntType(_), IntType(_)) => Trilean.True
-    case (IntType(_), _: SimpleType) => Trilean.False
+    case (IntegerType(_), _: SimpleType)  => Trilean.False
+    case (IntType(_), IntType(_))         => Trilean.True
+    case (IntType(_), _: SimpleType)      => Trilean.False
     case (NaturalType(_), NaturalType(_)) => Trilean.True
-    case (NaturalType(_), _: SimpleType) => Trilean.False
-    case (StringType(_), StringType(_)) => Trilean.True
-    case (StringType(_), _: SimpleType) => Trilean.False
+    case (NaturalType(_), _: SimpleType)  => Trilean.False
+    case (StringType(_), StringType(_))   => Trilean.True
+    case (StringType(_), _: SimpleType)   => Trilean.False
     case (Union(xs, meta), rhs) =>
-      if(xs.exists(lhs=>tryUnify(lhs, rhs).isTrue)) return Trilean.True
-      if(xs.forall(lhs=>tryUnify(lhs, rhs).isFalse)) return Trilean.False
+      if (xs.exists(lhs => tryUnify(lhs, rhs).isTrue)) return Trilean.True
+      if (xs.forall(lhs => tryUnify(lhs, rhs).isFalse)) return Trilean.False
       Trilean.Unknown
     case (lhs, Intersection(ys, meta)) =>
-      if(ys.exists(rhs=>tryUnify(lhs, rhs).isTrue)) return Trilean.True
-      if(ys.forall(rhs=>tryUnify(lhs, rhs).isFalse)) return Trilean.False
+      if (ys.exists(rhs => tryUnify(lhs, rhs).isTrue)) return Trilean.True
+      if (ys.forall(rhs => tryUnify(lhs, rhs).isFalse)) return Trilean.False
       Trilean.Unknown
     case _ =>
       Trilean.Unknown
@@ -49,5 +51,9 @@ case class Unify(lhs: CellRWOr[Term], rhs: CellRWOr[Term])(using ctx: Context) e
 }
 
 case object UnifyHandler extends Handler[ElabOps, Unify.type](Unify) {
-  override def run(c: Unify)(using ElabOps, SolverOps): Result = ???
+  override def run(c: Unify)(using ElabOps, SolverOps): Result = {
+    import c.{*, given}
+    if (rhs <:? lhs isTrue) return Result.Done
+    ???
+  }
 }
