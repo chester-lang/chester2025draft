df6330f84f7c670ac65ac3a6649aebe6a22dcbdc
澪 <mio@mio19.uk>
2025-05-15 14:02:45 +1200

    BlockElab

--- semantics/shared/src/main/scala/chester/elab/BlockElab.scala
+++ semantics/shared/src/main/scala/chester/elab/BlockElab.scala
@@ -1,5 +1,6 @@
 package chester.elab
 
+import chester.cell.CellEffects
 import chester.syntax.concrete.*
 import chester.syntax.core.*
 import chester.tyck.Context
@@ -9,17 +10,19 @@ case object BlockElab extends Kind {
   type Of = BlockElab
 }
 
-case class BlockElab(block: Block, ty: CellRWOr[Term])(using ops:SolverOps, ctx:Context) extends Constraint(BlockElab) with ConstraintTerm {
-    override def result: CellRW[BlockTerm] = newHole
-    given Context = ctx
+case class BlockElab(block: Block, ty: CellRWOr[Term])(using effects: CellEffects, elab: Elab, ops: SolverOps, ctx: Context)
+    extends Constraint(BlockElab)
+    with ConstraintTerm {
+  override def result: CellRW[BlockTerm] = newHole
+  given Context = ctx
+  given Elab = elab
+  given CellEffects = effects
 }
 
 case object BlockElabHandler extends Handler[ElabOps, BlockElab.type](BlockElab) {
-  override def run(c: BlockElab)(using ElabOps, SolverOps): Result = {
+  override def run(c: BlockElab)(using ElabOps, SolverOps): Result =
     ???
-  }
 
-  override def defaulting(c: BlockElab, level: DefaultingLevel)(using ElabOps, SolverOps): Unit = {
-    ???
-  }
+  override def defaulting(c: BlockElab, level: DefaultingLevel)(using ElabOps, SolverOps): Unit =
+    ()
 }
