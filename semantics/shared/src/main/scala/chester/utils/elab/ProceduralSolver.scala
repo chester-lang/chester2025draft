2589e2f4169d43b809ce2a02883dc132f4ba8629
澪 <mio@mio19.uk>
2025-05-15 14:54:16 +1200

    updates

--- semantics/shared/src/main/scala/chester/utils/elab/ProceduralSolver.scala
+++ semantics/shared/src/main/scala/chester/utils/elab/ProceduralSolver.scala
@@ -84,7 +84,7 @@ final class ProceduralSolver[Ops](val conf: HandlerConf[Ops])(using Ops) extends
       }
       if (updatedCells.nonEmpty) nothingChanged = false
     }
-    if (defaults.isEmpty && nothingChanged) {
+    if (defaults.isEmpty && nothingChanged && todo.isEmpty) {
       if(stable) return
       throw new IllegalStateException("cannot finish some constraints")
     }
