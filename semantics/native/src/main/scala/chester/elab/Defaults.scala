a8c137a221daf9c50b9662ea14aa2e687374c5b4
澪 <mio@mio19.uk>
2025-05-16 20:34:01 +1200

    why does scala-stm use reflect ?

--- semantics/native/src/main/scala/chester/elab/Defaults.scala
+++ semantics/native/src/main/scala/chester/elab/Defaults.scala
@@ -2,13 +2,8 @@ package chester.elab
 
 import chester.utils.elab.*
 
-import com.eed3si9n.ifdef.*
-
 object Defaults {
   given Elab = DefaultElabImpl
-  @ifndef("scalaNativeNoMultithread")
-  given SolverFactory = ConcurrentSolver // ProceduralSolver
-  @ifdef("scalaNativeNoMultithread")
   given SolverFactory = ProceduralSolver
   given HandlerConf[ElabOps] = DefaultSolverConf
 
