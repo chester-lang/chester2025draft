083a723090d3565db4c74621c577de247129bd71
澪 <mio@mio19.uk>
2025-05-21 00:23:42 +1200

    saving elab: add def

--- semantics/shared/src/main/scala/chester/elab/BlockElab.scala
+++ semantics/shared/src/main/scala/chester/elab/BlockElab.scala
@@ -6,8 +6,10 @@ import chester.syntax.concrete.*
 import chester.syntax.core.*
 import chester.tyck.{Context, ContextItem, TyAndVal, convertMeta}
 import chester.tyck.Tycker.MutableContext
-import chester.uniqid.Uniqid
+import chester.uniqid.{Uniqid, UniqidOf}
 import chester.utils.elab.*
+
+import scala.collection.mutable
 
 case object BlockElab extends Kind {
   type Of = BlockElab
@@ -29,37 +31,48 @@ case object BlockElabHandler extends Handler[ElabOps, BlockElab.type](BlockElab)
     val outerContext = c.context
     given context: MutableContext = new MutableContext(outerContext)
     var statements: Vector[StmtTerm] = Vector()
+    val defs: mutable.Queue[(defined: Identifier, ty: CellRW[Term], id: UniqidOf[LocalV], localv: LocalV)] = mutable.Queue()
     exprStatements.foreach {
-      case defstmt: LetDefStmt if defstmt.kind == LetDefType.Def => defstmt.defined match {
-        case DefinedPattern(PatternBind(name, meta)) => {
-          val ty = toTerm(newType)
-          val id = Uniqid.generate[LocalV]
-          val localv = LocalV(name.name, ty, id, convertMeta(meta))
-          ???
+      case defstmt: LetDefStmt if defstmt.kind == LetDefType.Def =>
+        defstmt.defined match {
+          case defined @ DefinedPattern(PatternBind(name, meta)) =>
+            val ty = newType
+            val id = Uniqid.generate[LocalV]
+            val localv = LocalV(name.name, toTerm(ty), id, convertMeta(meta))
+            val r = elab.collector.newSymbol(localv, id, defstmt, context.ctx)
+            context.update(_.add(ContextItem(name.name, id, localv, toTerm(ty), Some(r))))
+            defs.enqueue((name, ty, id, localv))
+          case _ => ???
         }
-        case _ => ???
-      }
       case _ => () // ignored
     }
     for (s <- exprStatements)
       s match {
-        case let: LetDefStmt if let.kind == LetDefType.Let =>
+        case let: LetDefStmt =>
           val pattern = let.defined
           val body = let.body.getOrElse { Reporter.report(???); ??? }
           pattern match {
             case DefinedPattern(pattern) =>
               val ty = toTerm(let.ty match {
                 case Some(ty) => given_Elab.inferType(ty).wellTyped
-                case None        => newType
+                case None     => newType
               })
               val wellTyped = toTerm(given_Elab.check(body, ty))
               pattern match {
                 case PatternBind(name, meta) =>
-                  val id = Uniqid.generate[LocalV]
-                  val localv = LocalV(name.name, ty, id, convertMeta(meta))
-                  val r = elab.collector.newSymbol(localv, id, let, context.ctx)
-                  context.update(_.add(ContextItem(name.name, id, localv, ty, Some(r))).knownAdd(id, TyAndVal(ty, wellTyped)))
-                  statements = statements :+ LetStmtTerm(localv, wellTyped, ty, convertMeta(let.meta))
+                  if (let.kind == LetDefType.Let) {
+                    val id = Uniqid.generate[LocalV]
+                    val localv = LocalV(name.name, ty, id, convertMeta(meta))
+                    val r = elab.collector.newSymbol(localv, id, let, context.ctx)
+                    context.update(_.add(ContextItem(name.name, id, localv, ty, Some(r))).knownAdd(id, TyAndVal(ty, wellTyped)))
+                    statements = statements :+ LetStmtTerm(localv, wellTyped, ty, convertMeta(let.meta))
+                  } else {
+                    val self = defs.dequeue()
+                    assume(self.defined == name)
+                    self.ty.fill(ty)
+                    context.update(_.knownAdd(self.id, TyAndVal(ty, wellTyped)))
+                    statements = statements :+ DefStmtTerm(self.localv, wellTyped, ty, convertMeta(let.meta))
+                  }
                 case _ => ???
               }
             case _ => ???
