e31878036d63a4042113c4b71c6b0f03b7fcf1f9
澪 <mio@mio19.uk>
2025-05-21 14:12:56 +1200

    PROCEDURAL_SOLVER for better debug?

--- semantics/jvm/src/main/scala/chester/elab/Defaults.scala
+++ semantics/jvm/src/main/scala/chester/elab/Defaults.scala
@@ -2,10 +2,10 @@ package chester.elab
 
 import chester.utils.elab.{ConcurrentSolver, HandlerConf, ProceduralSolver, SolverFactory}
 import chester.utils.ifNativeImage
-
+val PROCEDURAL_SOLVER = true
 object Defaults {
   given Elab = DefaultElabImpl
-  given SolverFactory = ifNativeImage(ProceduralSolver)(ConcurrentSolver)
+  given SolverFactory = if (PROCEDURAL_SOLVER) ProceduralSolver else ifNativeImage(ProceduralSolver)(ConcurrentSolver)
   given HandlerConf[ElabOps] = DefaultSolverConf
 
 }
