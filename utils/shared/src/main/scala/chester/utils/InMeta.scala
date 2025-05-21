429cec44bf8b707badb91eed967d069e7e70ffda
澪 <mio@mio19.uk>
2025-05-21 14:27:50 +1200

    define some equals?

--- utils/shared/src/main/scala/chester/utils/InMeta.scala
+++ utils/shared/src/main/scala/chester/utils/InMeta.scala
@@ -2,6 +2,7 @@ package chester.utils
 
 import upickle.default.*
 
+import scala.reflect.Selectable.reflectiveSelectable
 // allow to write, not allow read
 given HoldNotReadableRW: ReadWriter[InMeta[?]] =
   readwriter[InMetaRW].bimap(
@@ -9,13 +10,12 @@ given HoldNotReadableRW: ReadWriter[InMeta[?]] =
     _ => throw new UnsupportedOperationException("Cannot read HoldNotReadable")
   )
 
-case class InMeta[T](inner: T) extends AnyVal {
-  override def equals(obj: Any): Boolean = {
+case class InMeta[T](inner: T) {
+  override def equals(obj: Any): Boolean =
     obj match {
-      case InMeta(other) => inner == other
+      case InMeta(other) => (inner eq other) || inner == other
       case _             => false
     }
-  }
 }
 
 private case class InMetaRW() derives ReadWriter
