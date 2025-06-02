1a2cc9943d429b38b7622125e163fa29b98a15c5
澪 <mio@mio19.uk>
2025-06-02 21:43:17 +1200

    save

--- compiler/shared/src/main/scala/chester/targets/ts/TSBackend.scala
+++ compiler/shared/src/main/scala/chester/targets/ts/TSBackend.scala
@@ -3,6 +3,9 @@ package chester.targets.ts
 import chester.elab.*
 import chester.syntax.core.*
 import chester.targets.{Backend, Typescript}
+import chester.uniqid.UniqidOf
+
+import scala.collection.mutable
 import scala.language.implicitConversions
 
 implicit def metaConvert(x: Option[TermMeta]): Option[Meta] = x match {
@@ -10,14 +13,21 @@ implicit def metaConvert(x: Option[TermMeta]): Option[Meta] = x match {
   case None                 => None
 }
 
+case class TSContext(map: mutable.HashMap[UniqidOf[LocalV], String])
+
+object TSContext {
+  def create(map: mutable.HashMap[UniqidOf[LocalV], String] = new mutable.HashMap()): TSContext =
+    TSContext(map)
+}
+
 case object TSBackend extends Backend(Typescript) {
   def compileModule(mod: ZonkedTAST): Toplevel =
     ???
-  def compileExpr(term: Term): TSExpr = term match {
+  def compileExpr(term: Term)(using ctx: TSContext): TSExpr = term match {
     case IntTerm(value, meta) => DoubleExpr(value.toDouble, meta)
     case _                    => ???
   }
-  def compileStmt(stmt: StmtTerm): TSStmt = stmt match {
+  def compileStmt(stmt: StmtTerm)(using ctx: TSContext): TSStmt = stmt match {
     case _ => ???
   }
 }
