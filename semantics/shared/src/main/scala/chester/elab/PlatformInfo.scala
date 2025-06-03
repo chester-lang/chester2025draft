2c8a516d75b78ae7a909a932a2f65a2228ffe2a9
澪 <mio@mio19.uk>
2025-06-03 18:45:01 +1200

    introduce PlatformInfo

---
+++ semantics/shared/src/main/scala/chester/elab/PlatformInfo.scala
@@ -0,0 +1,16 @@
+package chester.elab
+
+trait PlatformInfo {
+  def IntMin: Long
+  def IntMax: Long
+}
+
+object JVMPlatformInfo extends PlatformInfo {
+  override def IntMin: Long = Int.MinValue.toLong
+  override def IntMax: Long = Int.MaxValue.toLong
+}
+
+object JSPlatformInfo extends PlatformInfo {
+  override def IntMin: Long = -9007199254740991L // -(2^53 - 1)
+  override def IntMax: Long = 9007199254740991L // 2^53 - 1
+}
