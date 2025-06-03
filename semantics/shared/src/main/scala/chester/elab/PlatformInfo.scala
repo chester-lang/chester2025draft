fb67a76ba5e4396bcbf771f629559b1f093a497c
澪 <mio@mio19.uk>
2025-06-03 19:04:37 +1200

    that is our current computer

--- semantics/shared/src/main/scala/chester/elab/PlatformInfo.scala
+++ semantics/shared/src/main/scala/chester/elab/PlatformInfo.scala
@@ -4,6 +4,14 @@ import chester.utils.Parameter
 import spire.math.UInt
 
 trait PlatformInfo {
+  def verify(): Unit = {
+
+    // requires at least 32 bits for Int
+    require(IntMin <= Int.MinValue.toLong, s"IntMin must be less than or equal to Int.MinValue, but got $IntMin vs ${Int.MinValue.toLong}")
+
+    require(IntMax >= Int.MaxValue.toLong)
+    require(UIntMax >= IntMax)
+  }
   // 128 bis platform, for example, is not supported currently
   def IntMin: Long
   def IntMax: Long
@@ -21,11 +29,13 @@ object JVMPlatformInfo extends PlatformInfo {
   override val IntMin: Long = Int.MinValue.toLong
   override val IntMax: Long = Int.MaxValue.toLong
   override val UIntMax: BigInt = UInt.MaxValue.toBigInt
+  verify()
 }
 
 object TypescriptPlatformInfo extends PlatformInfo {
   override val IntMin: Long = -9007199254740991L // -(2^53 - 1)
   override val IntMax: Long = 9007199254740991L // 2^53 - 1
+  verify()
 }
 
 val platformInfo: Parameter[PlatformInfo] = new Parameter[PlatformInfo]()
