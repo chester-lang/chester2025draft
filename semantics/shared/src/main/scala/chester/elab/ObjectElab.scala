0f36423b8f1b1deb8f9a13fb96bff1c61efb4818
澪 <mio@mio19.uk>
2025-06-04 21:34:36 +1200

    save

--- semantics/shared/src/main/scala/chester/elab/ObjectElab.scala
+++ semantics/shared/src/main/scala/chester/elab/ObjectElab.scala
@@ -1,16 +1,16 @@
 package chester.elab
 
+import chester.error.unreachable
 import chester.syntax.concrete.*
 import chester.syntax.core.*
 import chester.tyck.Context
 import chester.utils.elab.*
 
-
 case object ObjectElab extends Kind {
   type Of = ObjectElab
 }
 
-case class ObjectElab(obj: ObjectExpr, ty: CellRWOr[Term])(using elab: Elab, ops: SolverOps, ctx: Context)
+case class ObjectElab(obj: ObjectExpr, ty: CellRWOr[Term])(using elab0: Elab, ops: SolverOps, ctx: Context)
     extends Constraint(ObjectElab)
     with ConstraintTerm {
 
@@ -18,12 +18,18 @@ case class ObjectElab(obj: ObjectExpr, ty: CellRWOr[Term])(using elab: Elab, ops
 
   given context: Context = ctx
 
-  given Elab = elab
+  given elab: Elab = elab0
 }
 
 case object ObjectElabHandler extends Handler[ElabOps, ObjectElab.type](ObjectElab) {
-  override def run(c: ObjectElab)(using elab: ElabOps, solver: SolverOps): Result =
+  override def run(c: ObjectElab)(using elabOps: ElabOps, solver: SolverOps): Result = {
+    import c.{*, given}
+    val xs = obj.clauses.map {
+      case _: ObjectExprClause             => unreachable("ObjectExprClause should already be resolved")
+      case clause: ObjectExprClauseOnValue => (elab.infer(clause.key), elab.infer(clause.value))
+    }
     ???
+  }
 
-  override def canDefaulting(level: DefaultingLevel): Boolean = ???
+  override def canDefaulting(level: DefaultingLevel): Boolean = false
 }
