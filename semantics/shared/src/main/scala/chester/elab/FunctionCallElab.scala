8abe305421d8ea10aa7f25971786f5bf0a2dd721
澪 <mio@mio19.uk>
2025-06-07 17:46:34 +1200

    save FunctionCallElab

--- semantics/shared/src/main/scala/chester/elab/FunctionCallElab.scala
+++ semantics/shared/src/main/scala/chester/elab/FunctionCallElab.scala
@@ -6,6 +6,8 @@ import chester.elab.*
 import chester.error.Reporter
 import chester.utils.elab.*
 
+import scala.util.boundary
+
 case object FunctionCallElab extends Kind {
   type Of = FunctionCallElab
 }
@@ -26,27 +28,29 @@ case class FunctionCallElab(
 }
 
 case object FunctionCallElabHandler extends Handler[ElabOps, FunctionCallElab.type](FunctionCallElab) {
-  override def run(c: FunctionCallElab)(using ElabOps, SolverOps): Result = {
+  override def run(c: FunctionCallElab)(using ElabOps, SolverOps): Result = boundary {
     import c.{*, given}
     implicit var context: Context = c.context0
     val f = toTerm(c.f.wellTyped)
     val fTy = toTerm(c.f.ty) match {
       case meta: MetaTerm[?] =>
         // If the function type is a meta term, we need to assume it
-        return Result.Waiting(assumeCell(meta))
+        boundary.break(Result.Waiting(assumeCell(meta)))
       case f: FunctionType => f
       case term =>
         Reporter.report(???)
         result.fill(???)
-        return Result.Done
+        boundary.break(Result.Done)
     }
     val fTyImplicit = fTy.telescopes.map(_.implicitly)
     val callImplicit = call.telescopes.map(_.implicitly)
     if (fTyImplicit.length == callImplicit.length && fTyImplicit.zip(callImplicit).forall { case (x, y) => x == y }) {
       // simplest case
+      var callings: Vector[Calling] = Vector.empty
       for ((defTele, callTele) <- fTy.telescopes.zip(call.telescopes)) {
         if (defTele.args.length == callTele.args.length) {
           // check the simplest case
+          var calling: Vector[CallingArgTerm] = Vector.empty
           for ((defArg, callArg) <- defTele.args.zip(callTele.args))
             if (callArg.name.isEmpty || defArg.bind.exists(localv => localv.name == callArg.name.get.name)) {
               // simplest case, no different order of named arguments
@@ -56,14 +60,23 @@ case object FunctionCallElabHandler extends Handler[ElabOps, FunctionCallElab.ty
                 context =
                   context.add(ContextItem(bind.name, bind.uniqId, bind, defArg.ty)).knownAdd(bind.uniqId, TyAndVal(defArg.ty, toTerm(wellTyped)))
               }
+              calling = calling :+ CallingArgTerm(
+                toTerm(wellTyped),
+                defArg.ty,
+                name = defArg.bind.map(_.name),
+                meta = convertMeta(callArg.meta)
+              )
             } else {
               throw new UnsupportedOperationException("not implemented yet: different order of named arguments")
             }
+
+          assume(defTele.implicitly == callTele.implicitly)
+          callings = callings :+ Calling(calling, defTele.implicitly, meta = convertMeta(callTele.meta))
         } else {
           throw new UnsupportedOperationException("not implemented yet: default arguments in function calls")
         }
-        result.fill(FCallTerm(??? : Term, ??? : Seq[Calling], meta = convertMeta(call.meta)))
-        return Result.Done
+        result.fill(FCallTerm(f, callings, meta = convertMeta(call.meta)))
+        boundary.break(Result.Done)
       }
       ???
     } else {
