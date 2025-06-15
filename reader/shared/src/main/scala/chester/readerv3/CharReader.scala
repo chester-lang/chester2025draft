2ac312a124e763735d712e08af4b0d5fc5d3b384
澪 <mio@mio19.uk>
2025-06-15 22:23:08 +1200

    save reader works

--- reader/shared/src/main/scala/chester/readerv3/CharReader.scala
+++ reader/shared/src/main/scala/chester/readerv3/CharReader.scala
@@ -8,21 +8,21 @@ import chester.utils.getCodePoints
 import scala.collection.mutable.ArrayBuffer
 
 object CharReader {
-  private def reading(strings: Seq[String], source: Source, offset: Offset): Seq[StringChar] = {
-    if(strings.isEmpty) return Seq.empty
+  private def reading(strings: Seq[String], source: Source, offset: Offset): LazyList[StringChar] = {
+    if (strings.isEmpty) return LazyList.empty
     val head = strings.head
     val tail = strings.tail
     val chars = ArrayBuffer.empty[StringChar]
     var currentOffset = offset
-    for(codePoint <- getCodePoints(head)) {
+    for (codePoint <- getCodePoints(head)) {
       val begin = currentOffset
       currentOffset = begin.next(codePoint)
       chars += StringChar(codePoint, Span(source, SpanInFile(currentOffset.getPos, currentOffset.getPos)))
     }
-    LazyList.from(chars) ++ reading(tail, source, currentOffset)
+    LazyList.from(chars) #::: reading(tail, source, currentOffset)
   }
   def read(source: Source): Either[ParseError, Seq[StringChar]] = source.readContent match {
-    case Left(error) => Left(error)
+    case Left(error)    => Left(error)
     case Right(strings) => Right(reading(strings, source, source.offset))
   }
 }
