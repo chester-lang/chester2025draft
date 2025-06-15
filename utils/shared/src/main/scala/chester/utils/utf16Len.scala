ae710f424a77bfdb9ad8d51e20bf3d98ccd2ec7f
澪 <mio@mio19.uk>
2025-06-15 22:32:05 +1200

    save reader works

--- utils/shared/src/main/scala/chester/utils/utf16Len.scala
+++ utils/shared/src/main/scala/chester/utils/utf16Len.scala
@@ -9,3 +9,8 @@ extension (text: String) {
   def lenIsOne: Boolean =
     text.length == 1 || (text.length == 2 && Character.isHighSurrogate(text.charAt(0)) && Character.isLowSurrogate(text.charAt(1)))
 }
+
+extension (codePoint: Int) {
+  def utf16Len: Natural = Nat(Character.charCount(codePoint))
+  def unicodeLen: Natural = Nat(1)
+}
