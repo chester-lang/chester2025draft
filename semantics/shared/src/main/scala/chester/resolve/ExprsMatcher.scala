bfe17be6c85a7856df70736eea4180beaa1a38e6
澪 <mio@mio19.uk>
2025-06-15 22:46:27 +1200

    save desalt works

--- semantics/shared/src/main/scala/chester/resolve/ExprsMatcher.scala
+++ semantics/shared/src/main/scala/chester/resolve/ExprsMatcher.scala
@@ -5,9 +5,26 @@ import chester.syntax.concrete.Expr
 
 type ExprsMatcher = (Seq[Expr]) => Boolean
 
-case class ExprsParser[+A](parser: PartialFunction[Seq[Expr], A], prettyError: Option[(Seq[Expr]) => ParseError] = None) {
-  def map[B](f: A => B): ExprsParser[B] = copy(parser = parser.andThen(f))
+trait ExprsParser[+A] {
+  def parser: PartialFunction[Seq[Expr], A]
+  def prettyError: Option[(Seq[Expr]) => ParseError] = None
+
+  def map[B](f: A => B): ExprsParser[B] = new ExprsParser[B] {
+    override def parser: PartialFunction[Seq[Expr], B] = ExprsParser.this.parser.andThen(f)
+    override def prettyError: Option[(Seq[Expr]) => ParseError] = ExprsParser.this.prettyError
+  }
 }
+
+trait Expr1Parser[+A] extends ExprsParser[A] {
+  override final def parser: PartialFunction[Seq[Expr], A] = { case Seq(expr) => parse(expr) }
+  def parse(expr: Expr): A
+
+  override def map[B](f: A => B): ExprsParser[B] = new Expr1Parser[B] {
+    override def parse(expr: Expr): B = f(Expr1Parser.this.parse(expr))
+    override def prettyError: Option[(Seq[Expr]) => ParseError] = Expr1Parser.this.prettyError
+  }
+}
+
 type ExprsParserAny = ExprsParser[Any]
 
 object Exprs {
