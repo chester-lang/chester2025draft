a6ba867dcebcdb195f92bfdc95e3b17370e72641
澪 <mio@mio19.uk>
2025-06-15 22:42:54 +1200

    save desalt works

--- semantics/shared/src/main/scala/chester/resolve/ExprsMatcher.scala
+++ semantics/shared/src/main/scala/chester/resolve/ExprsMatcher.scala
@@ -3,11 +3,15 @@ package chester.resolve
 import chester.reader.ParseError
 import chester.syntax.concrete.Expr
 
-type ExprsMatcher = Function1[Seq[Expr], Boolean]
+type ExprsMatcher = (Seq[Expr]) => Boolean
 
-case class ExprsParser[+A](parser: PartialFunction[Seq[Expr], A], prettyError: Option[Function1[Seq[Expr], ParseError]] = None)
+case class ExprsParser[+A](parser: PartialFunction[Seq[Expr], A], prettyError: Option[(Seq[Expr]) => ParseError] = None) {
+  def map[B](f: A => B): ExprsParser[B] = copy(parser = parser.andThen(f))
+}
 type ExprsParserAny = ExprsParser[Any]
 
 object Exprs {
   def parse(exprs: Seq[Expr], parsers: Seq[ExprsParser[Any]]): Either[Option[ParseError], Seq[Any]] = ???
+  def combine[A, B](a: ExprsParser[A], b: ExprsParser[B]): ExprsParser[(A, B)] = ???
+  def combine[A, B, C](a: ExprsParser[A], b: ExprsParser[B], c: ExprsParser[C]): ExprsParser[(A, B, C)] = ???
 }
