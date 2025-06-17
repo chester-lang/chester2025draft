b20422371108ccefbb48adb35f300c88e9b1ecb6
澪 <mio@mio19.uk>
2025-06-17 20:50:51 +1200

    introduce ExprParser

--- semantics/shared/src/main/scala/chester/resolve/ExprParser.scala
+++ semantics/shared/src/main/scala/chester/resolve/ExprParser.scala
@@ -5,6 +5,22 @@ import chester.i18n.*
 import chester.syntax.Const
 
 import scala.util.parsing.combinator.Parsers
+import scala.util.parsing.input.{Position, Reader}
+
+case class SeqReader[+T](seq: Seq[T]) extends Reader[T] {
+  override def first: T = seq.head
+  override def atEnd: Boolean = seq.isEmpty
+  override def rest: SeqReader[T] = SeqReader(seq.tail)
+  override def drop(n: Int): SeqReader[T] = SeqReader(seq.drop(n))
+
+  override def pos: Position = new Position {
+    override def line: Int = ???
+
+    override def column: Int = ???
+
+    override protected def lineContents: String = ???
+  }
+}
 
 object ExprParser extends Parsers {
   type Elem = Expr
@@ -15,4 +31,12 @@ object ExprParser extends Parsers {
   def caseClause(meta: Option[ExprMeta] = None): Parser[DesaltCaseClause] = id(Const.Case) ~ any ~ id(Const.Arrow2) ~ any ^^ {
     case _ ~ pattern ~ _ ~ expr => DesaltCaseClause(pattern, expr, meta = meta)
   }
+  def acceptOpSeq: Parser[OpSeq] = accept(t"operation sequence", { case ops: OpSeq => ops })
+  def opseq[T](f: Option[ExprMeta] => Parser[T]): Parser[T] =
+    new Parser[T] {
+      def apply(in: Input): ParseResult[T] = {
+        val result = acceptOpSeq(in)
+        result.flatMapWithNext { case OpSeq(ops, meta) => _ => f(meta)(SeqReader(ops)) }
+      }
+    }.named("opseq")
 }
