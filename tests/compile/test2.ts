5bd572aa36c8dbae1c3b4c3746cef804c727c154
Mio <mio@mio19.uk>
2025-07-18 21:42:07 +1200

    ok

---
+++ tests/compile/test2.ts
@@ -0,0 +1,10 @@
+const a: number = 42;
+const a_1: number = a;
+const a_2: number = (Math.floor(Math.random() * 10) + 1);
+const a_3: number = a_2;
+const c: (_0: number) => number = (function(x) { return x+1; });
+const b: number = c(42);
+const d: (_0: number, _1: number) => number = (function(x, y) { return x + y; });
+const e: (_0: number, _1: number) => number = d;
+const f: number = e(1, 2);
+const func: (a: number, b: number) => number = function (a_4: number, b_1: number) { return a_4; };
