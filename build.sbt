af66da8ff9195c870f738eaa2f9eb1e06377cae8
Mio <mio@mio19.uk>
2025-07-31 23:46:29 +1200

    bumpScala

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 // reads env: NATIVE_IMAGE_OPTIONS, VERSION
 
-val scala3Nightly = "3.7.3-RC1-bin-20250708-8a14de3-NIGHTLY"
-val scala3Version = "3.7.2-RC1"
+val scala3Nightly = "3.7.3-RC1-bin-20250728-4b543e8-NIGHTLY"
+val scala3Version = "3.7.2"
 val scala3Lib = "3.6.4"
 val scala2Version = "2.13.16"
 val scala2Beta = "2.13.17-M1"
--- js-for-lua/index.js
+++ js-for-lua/index.js
@@ -1,9 +1,9 @@
 // Import from the project scala version
 // Current project Scala version is defined in build.sbt as scala3Version
-const SCALA_VERSION = '3.7.2-RC1'; // Update this when the project's Scala version changes
+const SCALA_VERSION = '3.7.2'; // Update this when the project's Scala version changes
 
 // Import the compiled Scala.js modules
-import * as Chester from './js/target/scala-3.7.2-RC1/js-for-lua-fastopt/main.mjs';
+import * as Chester from './js/target/scala-3.7.2/js-for-lua-fastopt/main.mjs';
 
 // Re-export the modules for bundling
 export default Chester; 
